1a8745e903cedcb8c953d0e33e42a2d5dfc86aee
Eric Reizas <tuo17224@temple.edu>
2023-11-07 12:57:35 -0500

    Configured style

--- gui.py
+++ gui.py
@@ -7,8 +7,12 @@ class GUI():
         self.__root = root
         self.__root.title('Schedule Compiler')
         #Can pick out style later
-        title_label = ttk.Label(self.__root, text = 'Schedule Compiler', font='Daytona 35 bold', justify=CENTER)
+        title_label = ttk.Label(self.__root, text = 'Schedule Compiler', font='Fixedsys 35 bold', justify=CENTER)
         title_label.pack(padx=5,pady=5)
+
+        self.__style = ttk.Style()
+        self.__style.configure('TButton', font = ('Courier',12,'bold'))
+        self.__style.configure('Header.TLabel', font = ('Courier',18,'bold'))
 
 if __name__=='__main__':
     root = Tk()
