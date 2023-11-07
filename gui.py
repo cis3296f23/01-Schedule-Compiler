810e75c0387bfe04e3bf08dac92e197e7d6ee4a7
Eric Reizas <tuo17224@temple.edu>
2023-11-07 12:48:47 -0500

    Added title

--- gui.py
+++ gui.py
@@ -3,7 +3,12 @@ from tkinter import ttk
 
 class GUI():
     def __init__(self,root):
-       pass
+        self.running = True
+        self.__root = root
+        self.__root.title('Schedule Compiler')
+        #Can pick out style later
+        title_label = ttk.Label(self.__root, text = 'Schedule Compiler', font='Daytona 35 bold', justify=CENTER)
+        title_label.pack(padx=5,pady=5)
 
 if __name__=='__main__':
     root = Tk()
