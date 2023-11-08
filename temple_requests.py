66e1b424c4ae501ff4ee043db449ed45621649ca
Eric Reizas <tuo17224@temple.edu>
2023-11-07 19:54:12 -0500

    Created function to retrieve curriculum for course of study from temple bulletin

--- temple_requests.py
+++ temple_requests.py
@@ -1,4 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 def get_curric(course_of_study):
-    pass
+    req = requests.get("https://bulletin.temple.edu/undergraduate/science-technology/" + '-'.join(course_of_study.lower().split()) + "/#requirementstext")
+    soup=BeautifulSoup(req.content,'html.parser')
+    requirements_html = soup.find('div',id='requirementstextcontainer')
+    courses_html = requirements_html.find_all('a',class_='bubblelink code')
+    curric = []
+    for c in courses_html:
+        course = c.text
+        curric.append(str(course))
+    return curric
+   
+        
+
+
+
+print(get_curric("Computer Science BS"))
