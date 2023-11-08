0b4e5f90682ad13e6f088630a829f5e03be9c18f
Eric Reizas <tuo17224@temple.edu>
2023-11-07 20:12:55 -0500

    Created function to retrieve all degree programs

--- temple_requests.py
+++ temple_requests.py
@@ -1,8 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_curric(course_of_study):
-    req = requests.get("https://bulletin.temple.edu/undergraduate/science-technology/" + '-'.join(course_of_study.lower().split()) + "/#requirementstext")
+def get_degree_programs():
+    req = requests.get("https://bulletin.temple.edu/academic-programs/")
+    soup = BeautifulSoup(req.content,'html.parser')
+    degree_programs_html = soup.find('tbody', class_='fixedTH',id='degree_body')
+    i = 0
+    degree_programs = []
+    for degree in degree_programs_html:
+        if not degree.text.isspace():
+            degree_programs.append(degree.text)
+    return degree_programs
+        
+
+def get_curric(degree_program):
+    req = requests.get("https://bulletin.temple.edu/undergraduate/science-technology/" + '-'.join(degree_program.lower().split()) + "/#requirementstext")
     soup=BeautifulSoup(req.content,'html.parser')
     requirements_html = soup.find('div',id='requirementstextcontainer')
     courses_html = requirements_html.find_all('a',class_='bubblelink code')
@@ -16,4 +28,5 @@ def get_curric(course_of_study):
 
 
 
-print(get_curric("Computer Science BS"))
+#print(get_curric("Computer Science BS"))
+print(get_degree_programs())
