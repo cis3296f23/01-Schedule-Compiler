21b93efebe2caff2b51bd5716d48f78646eb8f6b
Eric Reizas <tuo17224@temple.edu>
2023-11-08 07:22:47 -0500

    Added comments

--- temple_requests.py
+++ temple_requests.py
@@ -9,6 +9,7 @@ def get_degree_programs():
     degree_programs = []
     for degree in degree_programs_html:
         if not degree.text.isspace():
+            #can later split by level of program (the abbreviations that follow after some amount of spaces in each element)
             degree_programs.append(degree.text)
     return degree_programs
         
@@ -20,13 +21,9 @@ def get_curric(degree_program):
     courses_html = requirements_html.find_all('a',class_='bubblelink code')
     curric = []
     for c in courses_html:
-        course = c.text
-        curric.append(str(course))
+        #can later account for the "\xa0" in the middle of each, but printing each element produces the desired format
+        curric.append(c.text)
     return curric
    
-        
-
-
-
-#print(get_curric("Computer Science BS"))
 print(get_degree_programs())
+print(get_curric("Computer Science BS"))
