1d95e17826fd9e738932d88aa61f2388bcea9940
Becca Makenova <83739077+Be-Ma@users.noreply.github.com>
2023-11-15 14:45:35 -0500

    Initial scrollbar for the whole page and credit number input implementation
    To use:
    Only works when the window is expanded. Have to manually drag the scroll bar
    
    Needs improvement:
    1. Make it work on a small window as well.
    2. Improve the credit number verification

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import temple_requests
+from tkinter import Canvas, Scrollbar
 
 class GUI():
     def __init__(self,root):
@@ -11,13 +12,24 @@ class GUI():
         title_label = ttk.Label(self.__root, text = 'Schedule Compiler', font='Fixedsys 35 bold', justify=CENTER)
         title_label.pack(padx=5,pady=5)
 
+        generalFrame=ttk.Frame(self.__root)
+        generalFrame.pack(fill=BOTH, expand =1)
+
+        #Scrollbar implementation, incomplete
+        canv = Canvas(generalFrame)
+        canv.pack(side=LEFT,fill=BOTH,expand=1,anchor=CENTER)
+        mainScrollBar = ttk.Scrollbar(generalFrame,orient=VERTICAL,command=canv.yview)
+        mainScrollBar.pack(side='right',fill=Y)
+        canv.configure(yscrollcommand=mainScrollBar.set)
+        canv.bind('<Configure>', lambda e: canv.configure(scrollregion=canv.bbox("all")))
+        secondFrame = Frame(canv)
+        secondFrame.pack(fill=BOTH,expand=1)
+        canv.create_window((0,0), window=secondFrame, anchor = "nw")
+
         self.__style = ttk.Style()
         self.__style.configure('TButton', font = ('Courier',12,'bold'))
         self.__style.configure('Header.TLabel', font = ('Courier',18,'bold'))
-
-        generalFrame=ttk.Frame(self.__root)
-        generalFrame.pack(padx=5,pady=5)
-        self.build_general_frame(generalFrame)
+        self.build_general_frame(secondFrame)
     
     def build_general_frame(self,master):
         """
@@ -31,6 +43,7 @@ class GUI():
         self.degree_prog_entry=ttk.Entry(master,width=50)
         self.degree_prog_entry.grid(row=3,column=0)
         #only works for cst degree programs (need to fix)
+
         self.course_retrieval_btn = ttk.Button(master,text='Get Required Courses',command=self.get_courses)
         self.course_retrieval_btn.grid(row=4,column=0)
         ttk.Label(master,text="Courses in the curriculum:").grid(row=5,column=0)
@@ -50,9 +63,34 @@ class GUI():
         self.rmp_output = Text(master,width=80,height=5)
         self.rmp_output.grid(row=13,column=0)
 
+        #Entering number of credits
+        #ttk.Label(master, text = "Enter the number of credits").grid(row=14, column=0)
+        #self.credit_entry=ttk.Entry(master,width=30)
+        #self.credit_entry.grid(row=11,column=0)
+
+        #Enter the semester
+
+
+        # Entering number of credits
+        ttk.Label(master, text="Enter the number of credits").grid(row=14, column=0)
+        self.low_entry = ttk.Entry(master, width=15)
+        self.low_entry.grid(row=15, column=0, padx=5, pady=5)
+
+        ttk.Label(master, text="to").grid(row=15, column=1)
+        self.high_entry = ttk.Entry(master, width=15)
+        self.high_entry.grid(row=15, column=2, padx=5, pady=5)
+
+        submit_range_btn = ttk.Button(master, text="Submit Range", command=self.submit_range)
+        submit_range_btn.grid(row=16, column=0, columnspan=1, pady=10)
+
+    def submit_range():
+        low_value = int(self.low_entry.get())
+        high_value = int(self.high_entry.get())
+        print(f"Credit Number Range: {low_value} to {high_value}")
 
     def get_courses(self):
         self.retrieval_btn_output.insert(END,temple_requests.get_curric(self.degree_prog_entry.get()))
+
 
     def get_schedule_info(self):
         course = self.course_entry.get()
@@ -62,6 +100,14 @@ class GUI():
         prof = self.prof_entry.get()
         rating_info = temple_requests.get_rmp_data(prof)
         self.rmp_output.insert(END,"Professor " + prof + " has a rating of " + str(rating_info[0]) + " from " + str(rating_info[1]) + " reviews.")
+
+    #def select_num_credits(self):
+        #credits = self.credit_entry.get()
+
+    def submit_range(self):
+        low_value = int(self.low_entry.get())
+        high_value = int(self.high_entry.get())
+        print(f"Credit Number Range: {low_value} to {high_value}")
 
 if __name__=='__main__':
     root = Tk()
