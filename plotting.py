e4773240eaeeae081a58772da41250a89f18b30c
Eric Reizas <tuo17224@temple.edu>
2023-12-07 17:42:41 -0500

    Added colors and preparing of data

--- plotting.py
+++ plotting.py
@@ -1,10 +1,14 @@
 from numpy import arange, sin, pi
 import matplotlib
+import matplotlib.colors as mcolors
 matplotlib.use('WXAgg')
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.backends.backend_wx import NavigationToolbar2Wx
 from matplotlib.figure import Figure
 import wx
+from algo import Schedule
+import pandas as pd
+
 class CanvasPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
@@ -19,7 +23,11 @@ class CanvasPanel(wx.Panel):
         self.SetSizer(self.sizer)
         self.Fit()
 
-    def draw(self):
+    def draw(self,schedules:list[Schedule]):
+        # Predefined colors for consistency
+        colors = list(mcolors.TABLEAU_COLORS.values())
+        course_colors = {}
+
         def military_time_to_number(military_time):
             # Makes sure the time is in a 4-digit format
             military_time_str = f"{military_time:04d}"  
@@ -39,4 +47,23 @@ class CanvasPanel(wx.Panel):
             self.axes[i].clear()
             self.axes[i].invert_yaxis()
             labels_added = set()
+            # A dictionary to map days to numbers
+            day_to_num = {'Sunday': 1, 'Monday': 2, 'Tuesday': 3, 'Wednesday': 4, 'Thursday': 5, 'Friday': 6, 'Saturday': 7}
+            # Prepare data for each schedule
+            schedule_data_list = []
+            for my_schedule in schedules:
+                data = []
+                for section in my_schedule.sections:
+                    course_name = section['name']
+                    if course_name not in course_colors:
+                        course_colors[course_name] = colors[len(course_colors) % len(colors)]
+                    for day, timeslots in section['schedule'].days.items():
+                        for timeslot in timeslots:
+                            start_time, end_time = timeslot
+                            data.append({'Course Name': course_name, 'Day': day_to_num[day.capitalize()], 
+                                        'Start Time': military_time_to_number(start_time), 
+                                        'End Time': military_time_to_number(end_time)})
+                schedule_data_list.append(pd.DataFrame(data))
+            current_schedule = [0]  # Using a list to modify the current index inside the functions
+            
             self.axes[i].plot()
