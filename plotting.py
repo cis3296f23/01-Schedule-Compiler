f1ff9c4e8d73cb1900e974979cb7fd04d0c9a4ba
Eric Reizas <tuo17224@temple.edu>
2023-12-07 21:08:00 -0500

    Implemented notebook with pages for each potential roster

--- plotting.py
+++ plotting.py
@@ -8,24 +8,33 @@ from matplotlib.figure import Figure
 import wx
 import pandas as pd
 
+class MainFrame(wx.Frame):
+    def __init__(self,schedules):
+        wx.Frame.__init__(self,None, title="Potential Rosters")
+        main_panel = wx.Panel(self)
+        nb = wx.Notebook(main_panel)
+        panels = [] 
+        for s in range(len(schedules)):
+            panels.append(CanvasPanel(nb))
+            nb.AddPage(panels[s],"Roster " + str(s+1))
+            panels[s].draw(schedules,s)
+        sizer = wx.BoxSizer()
+        sizer.Add(nb, 1, wx.EXPAND)
+        main_panel.SetSizer(sizer)
+
 #didn't add the different pages yet
 class CanvasPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
         self.figure = Figure()
-        self.axes = []
-        """subplot takes three numbers either in int or tuple form
-            first number is num_rows, second is num_cols, and third is index into that
-            might be better to get rid of this loop and just do 111 for the parameter and draw the plots on different screens"""
-        for i in range(511,516):
-            self.axes.append(self.figure.add_subplot(i))
+        self.axes = self.figure.add_subplot(121)
         self.canvas = FigureCanvas(self, -1, self.figure)
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
         self.SetSizer(self.sizer)
         self.Fit()
 
-    def draw(self,schedules:list):
+    def draw(self,schedules:list, i:int):
         # Predefined colors for consistency
         colors = list(mcolors.TABLEAU_COLORS.values())
         course_colors = {}
@@ -76,26 +85,24 @@ class CanvasPanel(wx.Panel):
             # Handle legend
             ax.legend(title='Courses', bbox_to_anchor=(1.1, 1), loc='upper left')
     
-        for i in range(0,5):
-            # A dictionary to map days to numbers
-            day_to_num = {'Sunday': 1, 'Monday': 2, 'Tuesday': 3, 'Wednesday': 4, 'Thursday': 5, 'Friday': 6, 'Saturday': 7}
-            # Prepare data for each schedule
-            schedule_data_list = []
-            for my_schedule in schedules:
-                data = []
-                for section in my_schedule.sections:
-                    course_name = section['name']
-                    if course_name not in course_colors:
-                        course_colors[course_name] = colors[len(course_colors) % len(colors)]
-                    for day, timeslots in section['schedule'].days.items():
-                        for timeslot in timeslots:
-                            start_time, end_time = timeslot
-                            data.append({'Course Name': course_name, 'Day': day_to_num[day.capitalize()], 
-                                        'Start Time': military_time_to_number(start_time), 
-                                        'End Time': military_time_to_number(end_time)})
-                schedule_data_list.append(pd.DataFrame(data))
-            current_schedule = [i]  # Using a list to modify the current index inside the functions
-            # Initial draw
-            if current_schedule[0]<len(schedule_data_list):
-                draw_schedule(self.axes[i], schedule_data_list[current_schedule[0]], current_schedule[0]+1)
-            self.axes[i].plot()
+        # A dictionary to map days to numbers
+        day_to_num = {'Sunday': 1, 'Monday': 2, 'Tuesday': 3, 'Wednesday': 4, 'Thursday': 5, 'Friday': 6, 'Saturday': 7}
+        # Prepare data for each schedule
+        schedule_data_list = []
+        for my_schedule in schedules:
+            data = []
+            for section in my_schedule.sections:
+                course_name = section['name']
+                if course_name not in course_colors:
+                    course_colors[course_name] = colors[len(course_colors) % len(colors)]
+                for day, timeslots in section['schedule'].days.items():
+                    for timeslot in timeslots:
+                        start_time, end_time = timeslot
+                        data.append({'Course Name': course_name, 'Day': day_to_num[day.capitalize()], 
+                                    'Start Time': military_time_to_number(start_time), 
+                                    'End Time': military_time_to_number(end_time)})
+            schedule_data_list.append(pd.DataFrame(data))
+        # Initial draw
+        if i<len(schedule_data_list):
+            draw_schedule(self.axes, schedule_data_list[i], i+1)
+        self.axes.plot()
