326a12001f4c89422df49a8c5722680b50020340
Eric Reizas <tuo17224@temple.edu>
2024-05-13 14:04:55 -0400

    Added handling of attributes

--- temple_requests.py
+++ temple_requests.py
@@ -272,18 +272,20 @@ def get_course_sections_info(course_info : dict, term:str, term_code:str,subj:st
                 for day in days_of_the_week:
                     if meet_time_info[day]:
                         sched.add_timeslot(day,int(meet_time_info['beginTime']),int(meet_time_info['endTime']))
-            #term included in case we later want to cache info to reduce time used on requests for another schedule generation in the same session
             #partOfTerm included in case can schedule two courses with the same meeting times but in different parts of the semester
             sect_info = {'name':section['subject'] + ' ' + section['courseNumber'],'term':section['term'],'CRN':section['courseReferenceNumber'],'partOfTerm':section['partOfTerm'],
                          'seatsAvailable':section['seatsAvailable'],'maxEnrollment':section['maximumEnrollment'],
                          'creditHours':section['creditHourLow'] if section['creditHourLow'] else section['creditHourHigh'], 
                          'professor':professor,'profRating':rmp_info[0],'numReviews':rmp_info[1],'schedule':sched}
-            course = section['subject'] + ' ' + section['courseNumber']
+            course = section['subject'] + ' ' + section['courseNumber'] if not attr else attr
             if course not in course_info[term]:
                 course_info[term][course] = [sect_info]
             else:
                 course_info[term][course].append(sect_info)
-        course_info[term][subj + ' ' + course_num].sort(reverse=True,key=get_weighted_rating)
+        if subj: #if subj and course_num given
+            course_info[term][subj + ' ' + course_num].sort(reverse=True,key=get_weighted_rating)
+        else:
+            course_info[term][attr].sort(reverse=True,key=get_weighted_rating)
     else:
         return 'Invalid course or course not available'
     return ''
