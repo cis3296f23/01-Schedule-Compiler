8d84f8ecdec115db7b2e4f7fc14b84d6fcb6da5b
Eric Reizas <tuo17224@temple.edu>
2024-10-10 10:17:05 -0400

    Added documentation

--- app.py
+++ app.py
@@ -14,10 +14,16 @@ def index():
 
 @app.route("/degree_programs")
 def degree_programs():
+    """
+    Returns a dictionary of degree program names mapped to subdomain urls
+    """
     return get_degr_progs()
 
 @app.route("/curriculum")
 def curric():
+    """
+    Returns a list of Subject Code & Course Code and Course Name pairs
+    """
     curric_url = request.args.get('curric_url')
     if curric_url:
         return get_curric(urllib.parse.unquote(curric_url))
