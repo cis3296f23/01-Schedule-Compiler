390978c498ab91ad5c9a5760fd743723522cfdd6
Eric Reizas <tuo17224@temple.edu>
2024-10-10 10:52:06 -0400

    Added endpoint for getting parameter data codes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from temple_bulletin_api import get_degr_progs, get_curric
+from tuportal_api import get_param_data_codes
 import urllib.parse
 
 
@@ -21,14 +22,19 @@ def degree_programs():
 
 @app.route("/curriculum")
 def curric():
-    """
-    Returns a list of Subject Code & Course Code and Course Name pairs
-    """
-    curric_url = request.args.get('curric_url')
+    curric_url = request.args.get("curric_url")
     if curric_url:
         return get_curric(urllib.parse.unquote(curric_url))
     else:
         return "Link not valid or no link passed."
+
+@app.route("/param_data_codes")
+def param_data_codes():
+    param_type = request.args.get("param_type")
+    if param_type:
+        return get_param_data_codes(param_type)
+    else:
+        return "Parameter type not valid or no parameter passed."
 
 if __name__=="__main__":
     app.run(debug=True)
